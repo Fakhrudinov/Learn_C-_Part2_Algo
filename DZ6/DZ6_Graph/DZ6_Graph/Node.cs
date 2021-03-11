8c9f169b1f92f4bda11a863f28f6b313d5f3e503
Alexander Fakhrudinov <asbuka@gmail.com>
2021-03-11 08:55:00 +0300

    DZ6 finish

---
+++ DZ6/DZ6_Graph/DZ6_Graph/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DZ6_Graph
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public List<Edge> Edges { get; set; }
+
+        public bool IsPassed { get; set; }
+
+        public int WaveNumber { get; set; }
+
+        public Node(string nodeName)
+        {
+            Name = nodeName;
+            Edges = new List<Edge>();
+            IsPassed = false;
+        }
+    }
+}
---
+++ DZ6/DZ6_Graph/DZ6_Graph/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZ6_Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Alerxander Fakhrudinov = Александр Фахрудинов
+            * asbuka@gmail.com
+            * 
+            * Практическое задание
+            * Модифицируйте DFS и BFS из предыдущего урока, но уже для графа, также с выводом каждого шага
+            */
+
+            Console.WriteLine("Graph");
+
+            // [A]--3---[B]---2---[F]
+            //  |      /  \      / |
+            //  |     /    1    8  4
+            //  |    /      \  /   |
+            //  5   /       [E]   [H]
+            //  |  12       5      7
+            //  | /        /       |
+            // [C]--1---[D]---10--[G]
+
+            Node nodaA = new Node("A");
+            Node nodaB = new Node("B");
+            Node nodaC = new Node("C");
+            Node nodaD = new Node("D");
+            Node nodaE = new Node("E");
+            Node nodaF = new Node("F");
+            Node nodaG = new Node("G");
+            Node nodaH = new Node("H");
+
+            Edge.AddEdge(nodaA, nodaB, 3);
+            Edge.AddEdge(nodaA, nodaC, 5);
+            Edge.AddEdge(nodaB, nodaF, 2);
+            Edge.AddEdge(nodaB, nodaE, 1);
+            Edge.AddEdge(nodaB, nodaC, 12);
+            Edge.AddEdge(nodaF, nodaE, 8);
+            Edge.AddEdge(nodaF, nodaH, 4);
+            Edge.AddEdge(nodaE, nodaD, 5);
+            Edge.AddEdge(nodaH, nodaG, 7);
+            Edge.AddEdge(nodaC, nodaD, 1);
+            Edge.AddEdge(nodaD, nodaG, 10);
+
+
+            Edge.PrintNodesBFS(nodaG);
+
+            Edge.PrintNodesDFS(nodaG);
+
+            Console.Read();
+        }
+    }
+}
