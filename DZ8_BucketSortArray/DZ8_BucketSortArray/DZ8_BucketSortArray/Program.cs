5a15f1a886c446096b456a465b783040d5063ae7
Alexander Fakhrudinov <asbuka@gmail.com>
2021-03-17 06:44:07 +0300

    DZ8_start

---
+++ DZ8_BucketSortArray/DZ8_BucketSortArray/DZ8_BucketSortArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ8_BucketSortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
